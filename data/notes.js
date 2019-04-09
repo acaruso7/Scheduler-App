96573158cc1691d6556567a726ad9838c7c99c9f
alexcaruso <alex.caruso772@gmail.com>
2019-04-09 17:36:16 -0400

    cleaned up input checking code

--- data/notes.js
+++ data/notes.js
@@ -5,25 +5,18 @@ const {ObjectId} = require('mongodb');
 
 module.exports = {
     async getNoteById(id){
-        if(!id)
-        throw new Error("Id does't exist.");
-        // if(typeof id !=="string")
-        // throw new Error("Invalid note Id");
-        let objId = ObjectId(id);
+        if(!id) throw new Error("You must provide an id");
+        if(typeof id !== "string") throw new Error(`'id' must be a string. The inputted value is of type ${typeof id}`);
         const noteCollection = await notes();
-        const note = await noteCollection.findOne({_id:objId});
-        if(note === null)
-        throw new Error("Can't find this note.");
+        const note = await noteCollection.findOne({ _id: ObjectId(id) });
+        if(note === null) throw new Error("No note with that id");
         return note;
     },
-    async createNote(scheduleId,userId,user,comment){
-        //check if params exist
-        if(!scheduleId||!userId||!user||!comment)
-        throw new Error("Invalid notes information");
-        //check the type of params
-        if(typeof scheduleId !=="string"||typeof userId !== "string"||typeof user !=="string"||typeof comment!=="string")
-        throw new Error("Invalid type for notes params");
-        //create a note
+
+    async createNote(scheduleId, userId, user, comment){
+        if(!scheduleId||!userId||!user||!comment) throw new Error("One of scheduleId, userId, user, comment is undefined");
+        if(typeof scheduleId !=="string"||typeof userId !== "string"||typeof user !=="string"||typeof comment!=="string") 
+        throw new Error("One of scheduleId, userId, user, comment is not a string");
         let newNote ={
             scheduleId, 
             userId,
@@ -33,43 +26,36 @@ module.exports = {
         const noteCollection = await notes();
         let createOne = await noteCollection.insertOne(newNote);
         if(createOne.insertedCount === 0){
-            throw new Error("Failed to create a new note.");
+            throw new Error("Failed to create a new note");
         }
-        let newCreatedId = createOne.insertedId;
+        let newCreatedId = createOne.insertedId.toString();
         let note = await this.getNoteById(newCreatedId);
         return note;
     },
-    async removeNote(id){
-        if(!id)
-        throw new Error("Id does't exist.");
-        if(typeof id !=="string")
-        throw new Error("Invalid id");
 
+    async removeNote(id){
+        if(!id) throw new Error("You must input a note id");
+        if(typeof id !=="string") throw new Error(`'id' must be a string. The inputted id is of type ${typeof id}`);
         const noteCollection = await notes();
-        const objId = ObjectId(id);
-        const deletedOne = await noteCollection.findOneAndDelete({_id:objId});
-        if(deletedOne === null)
-        throw new Error("Can't delete this note.");
+        const deletedOne = await noteCollection.findOneAndDelete({ _id: ObjectId(id) });
+        if(deletedOne === null) throw new Error("Failed to delete this note.");
         return deletedOne;
     },
+
     async modifyNote(id,comment){
-        if(!id||typeof id !=="string")
-        throw new Error("Id does't exist.");
-        if(!comment||typeof comment !=="string")
-        throw new Error("Invalid comment info.");
+        if(!id||typeof id !=="string") throw new Error("id is undefined or not a string");
+        if(!comment||typeof comment !=="string") throw new Error("comment is undefined or not a string");
         const noteCollection = await notes();
-        const objId = ObjectId(id);
-        let preNote = await noteCollection.findOne({_id:objId});
+        let preNote = await noteCollection.findOne({ _id: ObjectId(id)});
         if(preNote === null)
-        throw new Error("Can't find previous note for this.");
+        throw new Error("Can't find note to be modified");
         let newNote = {
             $set:{
                 comment
             }
         }
-        const updateOne = await noteCollection.updateOne({_id:objId},newNote);
-        if(updateOne.modifiedCount === 0)
-        throw new Error("Can't modify this note.");
+        const updateOne = await noteCollection.updateOne({ _id: ObjectId(id) }, newNote);
+        if(updateOne.modifiedCount === 0) throw new Error("Unable to modify this note.");
         
         return await this.getNoteById(id);
     }
