98c8bb716e3020f3cbfce3379387d7bad6fd3e2e
Yangyang Liu <kingdom930414@gmail.com>
2019-05-02 17:43:51 -0400

    grid page

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -33,7 +33,51 @@ router.get("/", async (req, res) => {
 router.get("/:scheduleId", async (req, res) => {
   try {
     const schedule = await scheduleData.getScheduleByID(req.params.scheduleId);
-    res.status(200).json(schedule)
+
+    console.log(schedule);
+    let griddata = [];
+    let username =[];
+    for(let i=0;i<schedule.users.length;i++){
+      let user = await userData.get(schedule.users[i]);
+      username[i] =user.fullName;
+      griddata[i]=[];
+    }
+    let dates =[];
+    for(let j=0;j<schedule.dates.length;j++){
+      dates[j]=schedule.dates[j].toDateString();
+    }
+    for(let i=0;i<username.length;i++){
+      for(let j=0;j<dates.length;j++){
+        for(let k=0;k<schedule.responses.length;k++){
+          if(schedule.responses[k].user == schedule.users[i]){
+            for(let l=0;l<schedule.responses[k].availability.length;l++){
+              let date = schedule.responses[k].availability[l].date.toDateString();
+              if(date == dates[j]){
+                griddata[username[i]+dates[j]]=schedule.responses[k].availability[l].times;
+              }
+            }
+          }
+        }
+      }
+    }
+    let row = [];
+    for (let i = 0; i < username.length; i++) {
+        let col =[];
+        col[0]=username[i];
+        for (let j = 1; j < dates.length+1; j++) {
+          if(griddata[username[i]+dates[j-1]]==undefined){
+            col[j]="NotAvailable";
+          }else{
+            col[j]=griddata[username[i]+dates[j-1]];
+          }
+        }
+        row[i]=col;
+    }
+    console.log(row);
+    res.render('display',{
+      dates:dates,
+      row:row
+    });
   } catch(e) {
     console.log(e)
     res.status(500).send()
