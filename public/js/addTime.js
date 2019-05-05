ca7736624aaa6a456cefd57beec544c590a403d1
alexcaruso <alex.caruso772@gmail.com>
2019-05-04 20:20:19 -0400

    inviteForm input validation

--- public/js/addTime.js
+++ public/js/addTime.js
@@ -3,11 +3,11 @@
         let t = $("#datesContainer").children.length;
         document.getElementById("datesContainer").addEventListener('click', function(e){
             let tof = e.target;
-            if(tof.value=="yes"){
-                e.target.parentNode.parentNode.parentNode.parentNode.children[1].removeAttribute("style","display:none");
-            }
             if(tof.value=="no"){
                 e.target.parentNode.parentNode.parentNode.parentNode.children[1].setAttribute("style","display:none");
+            }
+            if(tof.value=="yes"){
+                e.target.parentNode.parentNode.parentNode.parentNode.children[1].removeAttribute("style","display:none");
             }
         }, true);
     })
---
+++ public/js/validateCheckboxes.js
@@ -0,0 +1,27 @@
+$(document).on('submit', function(e){
+    (function($){
+        $("#times").each(function(){
+            var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+            var boxes_checked = 0
+            var n = 0 // stores the date index (e.g. 3rd date, 5th date, etc)
+            for (let i=0; i<checkboxes.length; i++) {
+                if (checkboxes[i].checked) {
+                    boxes_checked ++ 
+                }
+                if (((i+1) % 15) == 0) { //check each group of 15 checkboxes
+                    n++ 
+                    if (boxes_checked == 0) {
+                        if(e.target.children[0].children[0].children[1].children[0].children[2*n-1].children[0].children[1].children[0].children[2].checked ){
+                            // if the 'No' box is checked, do nothing
+                        } else {
+                            alert('You must select at least one time for each date you are available')
+                            e.preventDefault()
+                        }
+                    } else {
+                        boxes_checked = 0
+                    }
+                }
+            }
+        })
+    }(jQuery))
+});
